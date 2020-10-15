0535b9fdeb46657c5f106d8ed6ecbc7688393980
sadra <sadra.delir@gmail.com>
2020-10-15 09:57:15 +0330

    adding a verse from saadi

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 public class Main{
     public static void main(String[] ghazanfar){
         System.out.println(ghazanfar[0]);
-
         
         // ye beyt az hafez
         int a = 10;
         System.out.println(a);
+
+
+        // ye beyt az saadi 
+        int b = 11;
+        System.out.println(b);
         
     }
 }
