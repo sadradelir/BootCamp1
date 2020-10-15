4fd94f3a1610c0b3687ed45b046132abd84089f2
sadra <sadra.delir@gmail.com>
2020-10-15 09:56:00 +0330

    adding a verse from hafez

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] ghazanfar){
         System.out.println(ghazanfar[0]);
+
+        
+        // ye beyt az hafez
+        int a = 10;
+        System.out.println(a);
+        
     }
 }
